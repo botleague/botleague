4d73ef7049f3212a4f4404127ca8960917712b01
Michael S. Emanuel <=>
2019-11-25 11:35:34 -0500

    updated shell scripts; run_visual works, run appears to hang

--- problems/deepdrive/unprotected_left/run.sh
+++ problems/deepdrive/unprotected_left/run.sh
@@ -8,14 +8,13 @@ python main.py \
       --server \
       --agent dagger_mobilenet_v2 \
       --use-latest-model \
-      --max-steps=1400 \
+      --max-steps=1440 \
       --max-episodes=1 \
       --record \
       --eval-only \
       --upload-gist \
       --scenario=2 \
       --map=kevindale_bare \
-      # --sync \
       --camera-rigs=default_rig_1080p \
       --image-resize-dims="[224,224,3]" \
       && echo "Sim finished running successfully. Check above for results."
--- problems/deepdrive/unprotected_left/run_visual.sh
+++ problems/deepdrive/unprotected_left/run_visual.sh
@@ -5,17 +5,15 @@ set -e
 export DEEPDRIVE_PUBLIC=true
 
 python main.py \
-      # --server \
-      --agent dagger_mobilenet_v2 \
-      --use-latest-model \
-      --max-steps=10 \
-      --max-episodes=1 \
-      --record \
-      --eval-only \
-      --upload-gist \
-      --scenario=2 \
-      --map=kevindale_bare \
-      # --sync \
-      --camera-rigs=default_rig_1080p \
-      --image-resize-dims="[224,224,3]" \
-      && echo "Sim finished running successfully. Check above for results."
+	--agent dagger_mobilenet_v2 \
+	--use-latest-model \
+	--max-steps=100 \
+	--max-episodes=1 \
+	--record \
+	--eval-only \
+	--upload-gist \
+	--scenario=2 \
+	--map=kevindale_bare \
+	--camera-rigs=default_rig_1080p \
+	--image-resize-dims="[224,224,3]"
+	&& echo "Sim finished running successfully. Check above for results."
